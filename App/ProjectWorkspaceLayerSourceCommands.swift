f3e0b808bfb2c166fce78e41836523152fa52653
woo642778-art <woo642778@gmail.com>
2026-08-05 23:57:44 -0700

    feat: edit nested camera and light layer sources

---
+++ App/ProjectWorkspaceLayerSourceCommands.swift
@@ -0,0 +1,36 @@
+import VertexCore
+import VertexProject
+
+@MainActor
+extension ProjectWorkspaceViewModel {
+    func setSelectedLayerSource(_ source: LayerSource) {
+        guard let layer = selectedLayer, layer.source != source else { return }
+        applyProjectOperation(.setLayerSource(layerID: layer.id, before: layer.source, after: source))
+    }
+
+    func setNestedSourceStart(seconds: Double) {
+        guard let layer = selectedLayer,
+              case .composition(let compositionID, _) = layer.source,
+              let composition = activeComposition,
+              let time = exactTime(seconds: seconds, frameRate: composition.frameRate) else { return }
+        setSelectedLayerSource(.composition(compositionID: compositionID, sourceStartTime: time))
+    }
+
+    func setNestedComposition(_ compositionID: VertexID) {
+        guard let layer = selectedLayer,
+              case .composition(_, let sourceStartTime) = layer.source else { return }
+        setSelectedLayerSource(.composition(compositionID: compositionID, sourceStartTime: sourceStartTime))
+    }
+
+    func setCameraFocalLength(_ value: Double) {
+        guard let layer = selectedLayer, case .camera(var settings) = layer.source else { return }
+        settings.focalLengthMillimeters = value
+        setSelectedLayerSource(.camera(settings))
+    }
+
+    func setLightIntensity(_ value: Double) {
+        guard let layer = selectedLayer, case .light(var settings) = layer.source else { return }
+        settings.intensity = value
+        setSelectedLayerSource(.light(settings))
+    }
+}
