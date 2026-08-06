ebf2e903068c37b008a24c2cd8679678239e400e
woo642778-art <woo642778@gmail.com>
2026-08-06 01:51:35 -0700

    fix: make startup readiness independent of phase number

---
+++ Sources/VertexCore/StartupReadinessPolicy.swift
@@ -0,0 +1,10 @@
+public enum StartupReadinessPolicy {
+    public static func shouldEnterApp(
+        architectureContracts: [ArchitectureContract],
+        milestone: Milestone
+    ) -> Bool {
+        !architectureContracts.isEmpty
+            && milestone.number >= 0
+            && !milestone.title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+}
