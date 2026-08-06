73aff2246ae7aa83226280eb5b6071c2cfb69d91
woo642778-art <woo642778@gmail.com>
2026-08-06 06:59:50 -0700

    feat: make vertexproject the editable document type

--- App/ProjectPackageFileDocument.swift
+++ App/ProjectPackageFileDocument.swift
@@ -2,19 +2,16 @@ import Foundation
 import SwiftUI
 import UniformTypeIdentifiers
 
-extension UTType {
-    static let afterEffectsProject = UTType(
-        exportedAs: "com.woo642778.aftereffects.project",
-        conformingTo: .package
-    )
-}
-
 struct ProjectPackageFileDocument: FileDocument, @unchecked Sendable {
-    static var readableContentTypes: [UTType] { [.afterEffectsProject] }
+    static var readableContentTypes: [UTType] { [.vertexProject] }
+    static var writableContentTypes: [UTType] { [.vertexProject] }
 
     private let wrapper: FileWrapper
 
     init(packageURL: URL) throws {
+        guard packageURL.pathExtension.lowercased() == ProjectDocumentTypes.canonicalExtension else {
+            throw CocoaError(.fileReadUnsupportedScheme)
+        }
         let wrapper = try FileWrapper(url: packageURL, options: [.immediate])
         wrapper.preferredFilename = packageURL.lastPathComponent
         self.wrapper = wrapper
