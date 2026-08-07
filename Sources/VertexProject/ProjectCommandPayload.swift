d4ed77d3fe9fca6ca4c8c7d2772464a0787db851
woo642778-art <woo642778@gmail.com>
2026-08-06 20:04:55 -0700

    feat: restore desired-state composition and layer payloads

--- Sources/VertexProject/ProjectCommandPayload.swift
+++ Sources/VertexProject/ProjectCommandPayload.swift
@@ -11,6 +11,29 @@ public enum ProjectCommandPayload: Equatable, Sendable {
     case setRenderBoolean(ProjectRenderBooleanParameter, value: Bool)
     case setOutputDimensions(width: Int, height: Int)
     case setProjectColor(ColorDescriptor)
+
+    case createComposition(ProjectComposition, ownedLayers: [ProjectLayer], index: Int)
+    case removeComposition(id: VertexID)
+    case duplicateComposition(sourceID: VertexID, newCompositionID: VertexID, newLayerIDs: [VertexID])
+    case renameComposition(id: VertexID, to: String)
+    case setCompositionDimensions(id: VertexID, width: Int, height: Int)
+    case setCompositionDuration(id: VertexID, duration: RationalTime)
+    case setCompositionFrameRate(id: VertexID, frameRate: RationalTime)
+    case setCompositionBackground(id: VertexID, color: ProjectRGBAColor)
+
+    case insertLayer(ProjectLayer, compositionID: VertexID, index: Int)
+    case removeLayer(id: VertexID)
+    case duplicateLayer(sourceID: VertexID, duplicateID: VertexID, index: Int)
+    case renameLayer(id: VertexID, to: String)
+    case reorderLayer(compositionID: VertexID, layerID: VertexID, toIndex: Int)
+    case setLayerEnabled(id: VertexID, value: Bool)
+    case setLayerLocked(id: VertexID, value: Bool)
+    case setLayerSolo(id: VertexID, value: Bool)
+    case setLayerTiming(id: VertexID, value: LayerTiming)
+    case setLayerTransform(id: VertexID, value: LayerTransform)
+    case setLayerBlendMode(id: VertexID, value: LayerBlendMode)
+    case setLayerSource(id: VertexID, value: LayerSource)
+    case setLayerOperations(id: VertexID, value: [LayerOperation])
 }
 
 public struct ProjectCommandRequest: Equatable, Sendable, Identifiable {
