9d4086f7c477aa55d474776fcb4d0098c1902928
woo642778-art <woo642778@gmail.com>
2026-08-11 07:53:30 -0700

    feat: route V17 effects through exact-time GPU renderer

--- App/NativeFrameEffectProcessor.swift
+++ App/NativeFrameEffectProcessor.swift
@@ -28,19 +28,17 @@ struct NativeFrameEffectProcessor {
             throw CompositionError.graphCompilationFailed("Native effect input is not a decodable image.")
         }
 
-        let output = try filteredImage(effect: request.effect, input: input).cropped(to: input.extent)
-        guard let data = Self.context.pngRepresentation(
-            of: output,
-            format: .RGBA8,
-            colorSpace: Self.colorSpace,
-            options: [:]
-        ) else {
+        let output = try filteredImage(effect: request.effect, input: input, exactTime: request.exactCompositionTime).cropped(to: input.extent)
+        guard let data = Self.context.pngRepresentation(of: output, format: .RGBA8, colorSpace: Self.colorSpace, options: [:]) else {
             throw CompositionError.graphCompilationFailed("Native effect output could not be encoded as PNG.")
         }
         return try PortableImage(data: data, format: .png, pixelSize: request.input.pixelSize)
     }
 
-    private func filteredImage(effect: ProjectEffect, input: CIImage) throws -> CIImage {
+    private func filteredImage(effect: ProjectEffect, input: CIImage, exactTime: VertexCore.RationalTime) throws -> CIImage {
+        if let generated = try V17GPUFrameEffectProcessor().filteredImage(effect: effect, input: input, exactTime: exactTime) {
+            return generated
+        }
         if let expanded = try NativeExpandedFrameEffectProcessor().filteredImage(effect: effect, input: input) {
             return expanded
         }
@@ -80,62 +78,47 @@ struct NativeFrameEffectProcessor {
         filter.setValue(input, forKey: kCIInputImageKey)
 
         switch effect.type {
-        case .gaussianBlur:
-            filter.setValue(try scalar(effect, GaussianBlurParameterID.radius), forKey: kCIInputRadiusKey)
-        case .fastBoxBlur:
-            filter.setValue(try scalar(effect, FastBoxBlurParameterID.radius), forKey: kCIInputRadiusKey)
+        case .gaussianBlur: filter.setValue(try scalar(effect, GaussianBlurParameterID.radius), forKey: kCIInputRadiusKey)
+        case .fastBoxBlur: filter.setValue(try scalar(effect, FastBoxBlurParameterID.radius), forKey: kCIInputRadiusKey)
         case .directionalBlur:
             filter.setValue(try scalar(effect, DirectionalBlurParameterID.radius), forKey: kCIInputRadiusKey)
             filter.setValue(try scalar(effect, DirectionalBlurParameterID.angle) * .pi / 180, forKey: kCIInputAngleKey)
-        case .sharpen:
-            filter.setValue(try scalar(effect, SharpenParameterID.sharpness), forKey: kCIInputSharpnessKey)
-        case .median:
-            break
+        case .sharpen: filter.setValue(try scalar(effect, SharpenParameterID.sharpness), forKey: kCIInputSharpnessKey)
+        case .median: break
         case .noiseReduction:
             filter.setValue(try scalar(effect, NoiseReductionParameterID.noiseLevel), forKey: "inputNoiseLevel")
             filter.setValue(try scalar(effect, NoiseReductionParameterID.sharpness), forKey: kCIInputSharpnessKey)
-        case .exposure:
-            filter.setValue(try scalar(effect, ExposureEffectParameterID.stops), forKey: kCIInputEVKey)
+        case .exposure: filter.setValue(try scalar(effect, ExposureEffectParameterID.stops), forKey: kCIInputEVKey)
         case .colorControls:
             filter.setValue(try scalar(effect, ColorControlsParameterID.brightness), forKey: kCIInputBrightnessKey)
             filter.setValue(try scalar(effect, ColorControlsParameterID.contrast), forKey: kCIInputContrastKey)
             filter.setValue(try scalar(effect, ColorControlsParameterID.saturation), forKey: kCIInputSaturationKey)
-        case .hueAdjust:
-            filter.setValue(try scalar(effect, HueAdjustParameterID.degrees) * .pi / 180, forKey: kCIInputAngleKey)
-        case .vibrance:
-            filter.setValue(try scalar(effect, VibranceParameterID.amount), forKey: kCIInputAmountKey)
-        case .gammaAdjust:
-            filter.setValue(try scalar(effect, GammaAdjustParameterID.power), forKey: "inputPower")
+        case .hueAdjust: filter.setValue(try scalar(effect, HueAdjustParameterID.degrees) * .pi / 180, forKey: kCIInputAngleKey)
+        case .vibrance: filter.setValue(try scalar(effect, VibranceParameterID.amount), forKey: kCIInputAmountKey)
+        case .gammaAdjust: filter.setValue(try scalar(effect, GammaAdjustParameterID.power), forKey: "inputPower")
         case .highlightShadow:
             filter.setValue(try scalar(effect, HighlightShadowParameterID.highlights), forKey: "inputHighlightAmount")
             filter.setValue(try scalar(effect, HighlightShadowParameterID.shadows), forKey: "inputShadowAmount")
-        case .sepiaTone:
-            filter.setValue(try scalar(effect, SepiaToneParameterID.intensity), forKey: kCIInputIntensityKey)
-        case .invert:
-            break
-        case .posterize:
-            filter.setValue(try scalar(effect, PosterizeParameterID.levels), forKey: "inputLevels")
+        case .sepiaTone: filter.setValue(try scalar(effect, SepiaToneParameterID.intensity), forKey: kCIInputIntensityKey)
+        case .invert: break
+        case .posterize: filter.setValue(try scalar(effect, PosterizeParameterID.levels), forKey: "inputLevels")
         case .mosaic:
             filter.setValue(try scalar(effect, MosaicParameterID.scale), forKey: kCIInputScaleKey)
             filter.setValue(CIVector(x: input.extent.midX, y: input.extent.midY), forKey: kCIInputCenterKey)
-        case .findEdges:
-            filter.setValue(try scalar(effect, FindEdgesParameterID.intensity), forKey: kCIInputIntensityKey)
+        case .findEdges: filter.setValue(try scalar(effect, FindEdgesParameterID.intensity), forKey: kCIInputIntensityKey)
         case .glow:
             filter.setValue(try scalar(effect, GlowParameterID.radius), forKey: kCIInputRadiusKey)
             filter.setValue(try scalar(effect, GlowParameterID.intensity), forKey: kCIInputIntensityKey)
         case .vignette:
             filter.setValue(try scalar(effect, VignetteParameterID.radius), forKey: kCIInputRadiusKey)
             filter.setValue(try scalar(effect, VignetteParameterID.intensity), forKey: kCIInputIntensityKey)
-        case .cartoon:
-            break
+        case .cartoon: break
         case .twirl:
             filter.setValue(CIVector(x: input.extent.midX, y: input.extent.midY), forKey: kCIInputCenterKey)
             filter.setValue(try scalar(effect, TwirlParameterID.radius), forKey: kCIInputRadiusKey)
             filter.setValue(try scalar(effect, TwirlParameterID.angle) * .pi / 180, forKey: kCIInputAngleKey)
-        case .depthMap, .cutout, .upscale, .restore:
-            break
-        default:
-            break
+        case .depthMap, .cutout, .upscale, .restore: break
+        default: break
         }
 
         guard let output = filter.outputImage else {
