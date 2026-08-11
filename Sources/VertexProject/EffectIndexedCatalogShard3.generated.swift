9966b671b50c8ed3eca23690a824279d0a9ae3ca
woo642778-art <woo642778@gmail.com>
2026-08-11 02:21:07 -0700

    data: add effect catalog shard 3

---
+++ Sources/VertexProject/EffectIndexedCatalogShard3.generated.swift
@@ -0,0 +1,201 @@
+enum EffectIndexedCatalogShard3 {
+    static let offset = 588
+    static let raw = """
+Boris FX Continuum|BCC Transitions|BCC Lens Flash
+Boris FX Continuum|BCC Transitions|BCC Light Wipe
+Boris FX Continuum|BCC Transitions|BCC Multi-Stretch Wipe
+Boris FX Continuum|BCC Transitions|BCC Multi-Stripe Wipe
+Boris FX Continuum|BCC Transitions|BCC Particle Illusion Dissolve
+Boris FX Continuum|BCC Transitions|BCC RGB Blur Dissolve
+Boris FX Continuum|BCC Transitions|BCC RGB Displacement Dissolve
+Boris FX Continuum|BCC Transitions|BCC Ribbon Wipe
+Boris FX Continuum|BCC Transitions|BCC Rings Wipe
+Boris FX Continuum|BCC Transitions|BCC Ripple Dissolve
+Boris FX Continuum|BCC Transitions|BCC Tile Wipe
+Boris FX Continuum|BCC Transitions|BCC Tritone Dissolve
+Boris FX Continuum|BCC Transitions|BCC Twister
+Boris FX Continuum|BCC Transitions|BCC Vector Blur Dissolve
+Boris FX Continuum|BCC Transitions|BCC Water Waves Dissolve
+Boris FX Continuum|BCC VR|BCC VR Blur
+Boris FX Continuum|BCC VR|BCC VR Flicker Fixer
+Boris FX Continuum|BCC VR|BCC VR Insert
+Boris FX Continuum|BCC VR|BCC VR Reorient
+Boris FX Continuum|BCC VR|BCC VR Sharpen
+Boris FX Continuum|BCC Warp|BCC+ Polar Warp
+Boris FX Continuum|BCC Warp|BCC+ ST Map
+Boris FX Continuum|BCC Warp|BCC Bulge
+Boris FX Continuum|BCC Warp|BCC Curl
+Boris FX Continuum|BCC Warp|BCC Displacement Map
+Boris FX Continuum|BCC Warp|BCC Morph
+Boris FX Continuum|BCC Warp|BCC Polar Displacement
+Boris FX Continuum|BCC Warp|BCC Ripple
+Boris FX Continuum|BCC Warp|BCC Turbulence
+Boris FX Continuum|BCC Warp|BCC Twirl
+Boris FX Continuum|BCC Warp|BCC Vector Displacement
+Boris FX Continuum|BCC Warp|BCC Video Morph
+Boris FX Continuum|BCC Warp|BCC Warp
+Boris FX Continuum|BCC Warp|BCC Wave
+BCC obsolete / legacy|Legacy / obsolete|BCC Alpha Pixel Noise
+BCC obsolete / legacy|Legacy / obsolete|BCC Alpha Spotlight
+BCC obsolete / legacy|Legacy / obsolete|BCC Beauty Studio
+BCC obsolete / legacy|Legacy / obsolete|BCC Blur Dissolve
+BCC obsolete / legacy|Legacy / obsolete|BCC Boost Blend
+BCC obsolete / legacy|Legacy / obsolete|BCC Brick
+BCC obsolete / legacy|Legacy / obsolete|BCC Cloth
+BCC obsolete / legacy|Legacy / obsolete|BCC Clouds
+BCC obsolete / legacy|Legacy / obsolete|BCC Color Palette
+BCC obsolete / legacy|Legacy / obsolete|BCC Comet
+BCC obsolete / legacy|Legacy / obsolete|BCC Corner Pin
+BCC obsolete / legacy|Legacy / obsolete|BCC Crash Zoom Dissolve
+BCC obsolete / legacy|Legacy / obsolete|BCC Cross Glitch
+BCC obsolete / legacy|Legacy / obsolete|BCC Cross Zoom
+BCC obsolete / legacy|Legacy / obsolete|BCC DeGrain
+BCC obsolete / legacy|Legacy / obsolete|BCC DeNoise
+BCC obsolete / legacy|Legacy / obsolete|BCC Directional Blur
+BCC obsolete / legacy|Legacy / obsolete|BCC DV Fixer
+BCC obsolete / legacy|Legacy / obsolete|BCC DVE Basic
+BCC obsolete / legacy|Legacy / obsolete|BCC DVE
+BCC obsolete / legacy|Legacy / obsolete|BCC Edge Cleaner
+BCC obsolete / legacy|Legacy / obsolete|BCC Fast Film Glow Dissolve
+BCC obsolete / legacy|Legacy / obsolete|BCC Fast Film Glow
+BCC obsolete / legacy|Legacy / obsolete|BCC Fast Film Process
+BCC obsolete / legacy|Legacy / obsolete|BCC Film Damage
+BCC obsolete / legacy|Legacy / obsolete|BCC Film Glow Dissolve
+BCC obsolete / legacy|Legacy / obsolete|BCC Film Glow
+BCC obsolete / legacy|Legacy / obsolete|BCC Film Grain
+BCC obsolete / legacy|Legacy / obsolete|BCC Film Process
+BCC obsolete / legacy|Legacy / obsolete|BCC Fire
+BCC obsolete / legacy|Legacy / obsolete|BCC Flutter Cut
+BCC obsolete / legacy|Legacy / obsolete|BCC Fractal Noise
+BCC obsolete / legacy|Legacy / obsolete|BCC Gaussian Blur
+BCC obsolete / legacy|Legacy / obsolete|BCC Glare
+BCC obsolete / legacy|Legacy / obsolete|BCC Glint
+BCC obsolete / legacy|Legacy / obsolete|BCC Glitter
+BCC obsolete / legacy|Legacy / obsolete|BCC Glow Matte
+BCC obsolete / legacy|Legacy / obsolete|BCC Glow
+BCC obsolete / legacy|Legacy / obsolete|BCC Granite
+BCC obsolete / legacy|Legacy / obsolete|BCC LED
+BCC obsolete / legacy|Legacy / obsolete|BCC Lens Blur
+BCC obsolete / legacy|Legacy / obsolete|BCC Lens Flare Adv
+BCC obsolete / legacy|Legacy / obsolete|BCC Lens Flare
+BCC obsolete / legacy|Legacy / obsolete|BCC Lens Transition
+BCC obsolete / legacy|Legacy / obsolete|BCC Light Leaks Dissolve
+BCC obsolete / legacy|Legacy / obsolete|BCC Light Leaks
+BCC obsolete / legacy|Legacy / obsolete|BCC Light Matte
+BCC obsolete / legacy|Legacy / obsolete|BCC Light Wrap
+BCC obsolete / legacy|Legacy / obsolete|BCC Light Zoom
+BCC obsolete / legacy|Legacy / obsolete|BCC Linear Color Key
+BCC obsolete / legacy|Legacy / obsolete|BCC Linear Luma Key
+BCC obsolete / legacy|Legacy / obsolete|BCC Linear Wipe
+BCC obsolete / legacy|Legacy / obsolete|BCC Magic Sharp
+BCC obsolete / legacy|Legacy / obsolete|BCC Mosaic
+BCC obsolete / legacy|Legacy / obsolete|BCC Motion Blur
+BCC obsolete / legacy|Legacy / obsolete|BCC Motion Key
+BCC obsolete / legacy|Legacy / obsolete|BCC Noise Map
+BCC obsolete / legacy|Legacy / obsolete|BCC Noise Reduction
+BCC obsolete / legacy|Legacy / obsolete|BCC Optical Flow
+BCC obsolete / legacy|Legacy / obsolete|BCC Particle System
+BCC obsolete / legacy|Legacy / obsolete|BCC Prism Dissolve
+BCC obsolete / legacy|Legacy / obsolete|BCC Prism
+BCC obsolete / legacy|Legacy / obsolete|BCC Pyramid Blur
+BCC obsolete / legacy|Legacy / obsolete|BCC Radial Blur
+BCC obsolete / legacy|Legacy / obsolete|BCC Radial Wipe
+BCC obsolete / legacy|Legacy / obsolete|BCC Rays Cartoon
+BCC obsolete / legacy|Legacy / obsolete|BCC Rays Dissolve
+BCC obsolete / legacy|Legacy / obsolete|BCC Rays
+BCC obsolete / legacy|Legacy / obsolete|BCC Rays Radiant Spotlight
+BCC obsolete / legacy|Legacy / obsolete|BCC Rays Ring
+BCC obsolete / legacy|Legacy / obsolete|BCC Rays Ripply
+BCC obsolete / legacy|Legacy / obsolete|BCC Rays Streaky
+BCC obsolete / legacy|Legacy / obsolete|BCC Rays Textured
+BCC obsolete / legacy|Legacy / obsolete|BCC Rays Wedge
+BCC obsolete / legacy|Legacy / obsolete|BCC Rectangular Wipe
+BCC obsolete / legacy|Legacy / obsolete|BCC Reptilian
+BCC obsolete / legacy|Legacy / obsolete|BCC RGB Blur Dissolve
+BCC obsolete / legacy|Legacy / obsolete|BCC Rock
+BCC obsolete / legacy|Legacy / obsolete|BCC Rough Glow
+BCC obsolete / legacy|Legacy / obsolete|BCC Safe Colors
+BCC obsolete / legacy|Legacy / obsolete|BCC Sequencer
+BCC obsolete / legacy|Legacy / obsolete|BCC Smooth Tone
+BCC obsolete / legacy|Legacy / obsolete|BCC Sparks
+BCC obsolete / legacy|Legacy / obsolete|BCC Spill Remover
+BCC obsolete / legacy|Legacy / obsolete|BCC Spin Blur Dissolve
+BCC obsolete / legacy|Legacy / obsolete|BCC Spiral Blur
+BCC obsolete / legacy|Legacy / obsolete|BCC Star Matte
+BCC obsolete / legacy|Legacy / obsolete|BCC Stars
+BCC obsolete / legacy|Legacy / obsolete|BCC Steel Plate
+BCC obsolete / legacy|Legacy / obsolete|BCC Super Blend
+BCC obsolete / legacy|Legacy / obsolete|BCC Swish Pan
+BCC obsolete / legacy|Legacy / obsolete|BCC Textured Wipe
+BCC obsolete / legacy|Legacy / obsolete|BCC Two Strip Color
+BCC obsolete / legacy|Legacy / obsolete|BCC Two Way Key
+BCC obsolete / legacy|Legacy / obsolete|BCC UpRez
+BCC obsolete / legacy|Legacy / obsolete|BCC Veined Marble
+BCC obsolete / legacy|Legacy / obsolete|BCC Velocity Remap
+BCC obsolete / legacy|Legacy / obsolete|BCC Video Glitch
+BCC obsolete / legacy|Legacy / obsolete|BCC Vignette
+BCC obsolete / legacy|Legacy / obsolete|BCC Vignette Wipe
+BCC obsolete / legacy|Legacy / obsolete|BCC Weave
+BCC obsolete / legacy|Legacy / obsolete|BCC WitnessProtection
+BCC obsolete / legacy|Legacy / obsolete|BCC Wood Grain
+BCC obsolete / legacy|Legacy / obsolete|BCC Wooden Planks
+BCC obsolete / legacy|Legacy / obsolete|BCC Z Space I
+BCC obsolete / legacy|Legacy / obsolete|BCC Z Space II
+BCC obsolete / legacy|Legacy / obsolete|BCC Z Space III
+Boris FX Sapphire|Sapphire 2026.5|S_AdvancedDefocus
+Boris FX Sapphire|Sapphire 2026.5|S_Aurora
+Boris FX Sapphire|Sapphire 2026.5|S_AutoPaint
+Boris FX Sapphire|Sapphire 2026.5|S_BandPass
+Boris FX Sapphire|Sapphire 2026.5|S_Beauty
+Boris FX Sapphire|Sapphire 2026.5|S_BleachBypass
+Boris FX Sapphire|Sapphire 2026.5|S_Blur
+Boris FX Sapphire|Sapphire 2026.5|S_BlurChannels
+Boris FX Sapphire|Sapphire 2026.5|S_BlurChroma
+Boris FX Sapphire|Sapphire 2026.5|S_BlurDirectional
+Boris FX Sapphire|Sapphire 2026.5|S_BlurMoCurves
+Boris FX Sapphire|Sapphire 2026.5|S_BlurMotion
+Boris FX Sapphire|Sapphire 2026.5|S_BokehLights
+Boris FX Sapphire|Sapphire 2026.5|S_BrushChalk
+Boris FX Sapphire|Sapphire 2026.5|S_BrushOil
+Boris FX Sapphire|Sapphire 2026.5|S_CardFlip
+Boris FX Sapphire|Sapphire 2026.5|S_Cartoon
+Boris FX Sapphire|Sapphire 2026.5|S_CartoonPaint
+Boris FX Sapphire|Sapphire 2026.5|S_Caustics
+Boris FX Sapphire|Sapphire 2026.5|S_ChannelSwitcher
+Boris FX Sapphire|Sapphire 2026.5|S_ClampChroma
+Boris FX Sapphire|Sapphire 2026.5|S_Clouds
+Boris FX Sapphire|Sapphire 2026.5|S_CloudsColorSmooth
+Boris FX Sapphire|Sapphire 2026.5|S_CloudsMultColor
+Boris FX Sapphire|Sapphire 2026.5|S_CloudsPerspective
+Boris FX Sapphire|Sapphire 2026.5|S_CloudsPsyko
+Boris FX Sapphire|Sapphire 2026.5|S_CloudsVortex
+Boris FX Sapphire|Sapphire 2026.5|S_ColorFuse
+Boris FX Sapphire|Sapphire 2026.5|S_Convolve
+Boris FX Sapphire|Sapphire 2026.5|S_ConvolveComp
+Boris FX Sapphire|Sapphire 2026.5|S_Crosshatch
+Boris FX Sapphire|Sapphire 2026.5|S_CutToDissolve
+Boris FX Sapphire|Sapphire 2026.5|S_Deband
+Boris FX Sapphire|Sapphire 2026.5|S_DefocusPrism
+Boris FX Sapphire|Sapphire 2026.5|S_Diffuse
+Boris FX Sapphire|Sapphire 2026.5|S_DigitalDamage
+Boris FX Sapphire|Sapphire 2026.5|S_Dissolve
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveAutoPaint
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveBlur
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveBubble
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveDefocus
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveDiffuse
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveDigitalDamage
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveDistort
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveEdgeRays
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveFilm
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveFlashbulbs
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveGlare
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveGlint
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveGlintRainbow
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveGlow
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveLensFlare
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveLightLeak
+Boris FX Sapphire|Sapphire 2026.5|S_DissolveLuma
+Boris FX Sapphire|Sapphire 2026.5|S_DissolvePixelMosh
+"""
+}
