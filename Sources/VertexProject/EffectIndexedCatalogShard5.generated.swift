ec124df35a8a48d9fd7173f42afc00331401c824
woo642778-art <woo642778@gmail.com>
2026-08-11 02:22:24 -0700

    data: add effect catalog shard 5

---
+++ Sources/VertexProject/EffectIndexedCatalogShard5.generated.swift
@@ -0,0 +1,201 @@
+enum EffectIndexedCatalogShard5 {
+    static let offset = 980
+    static let raw = """
+Boris FX Sapphire|Sapphire 2026.5|S_WarpPolar
+Boris FX Sapphire|Sapphire 2026.5|S_WarpPuddle
+Boris FX Sapphire|Sapphire 2026.5|S_WarpPuff
+Boris FX Sapphire|Sapphire 2026.5|S_WarpRepeat
+Boris FX Sapphire|Sapphire 2026.5|S_WarpTransform
+Boris FX Sapphire|Sapphire 2026.5|S_WarpVortex
+Boris FX Sapphire|Sapphire 2026.5|S_WarpWaves
+Boris FX Sapphire|Sapphire 2026.5|S_WarpWaves2
+Boris FX Sapphire|Sapphire 2026.5|S_WhipLash
+Boris FX Sapphire|Sapphire 2026.5|S_WipeBlobs
+Boris FX Sapphire|Sapphire 2026.5|S_WipeBubble
+Boris FX Sapphire|Sapphire 2026.5|S_WipeCells
+Boris FX Sapphire|Sapphire 2026.5|S_WipeChecker
+Boris FX Sapphire|Sapphire 2026.5|S_WipeCircle
+Boris FX Sapphire|Sapphire 2026.5|S_WipeClock
+Boris FX Sapphire|Sapphire 2026.5|S_WipeClouds
+Boris FX Sapphire|Sapphire 2026.5|S_WipeDiffuse
+Boris FX Sapphire|Sapphire 2026.5|S_WipeDots
+Boris FX Sapphire|Sapphire 2026.5|S_WipeDoubleWedge
+Boris FX Sapphire|Sapphire 2026.5|S_WipeFlux
+Boris FX Sapphire|Sapphire 2026.5|S_WipeFourWedges
+Boris FX Sapphire|Sapphire 2026.5|S_WipeLine
+Boris FX Sapphire|Sapphire 2026.5|S_WipeMoire
+Boris FX Sapphire|Sapphire 2026.5|S_WipePixelate
+Boris FX Sapphire|Sapphire 2026.5|S_WipePlasma
+Boris FX Sapphire|Sapphire 2026.5|S_WipePointalize
+Boris FX Sapphire|Sapphire 2026.5|S_WipeRectangle
+Boris FX Sapphire|Sapphire 2026.5|S_WipeRings
+Boris FX Sapphire|Sapphire 2026.5|S_WipeStar
+Boris FX Sapphire|Sapphire 2026.5|S_WipeStripes
+Boris FX Sapphire|Sapphire 2026.5|S_WipeTiles
+Boris FX Sapphire|Sapphire 2026.5|S_WipeWeave
+Boris FX Sapphire|Sapphire 2026.5|S_WipeWedge
+Boris FX Sapphire|Sapphire 2026.5|S_ZBlur
+Boris FX Sapphire|Sapphire 2026.5|S_ZComp
+Boris FX Sapphire|Sapphire 2026.5|S_ZConvolve
+Boris FX Sapphire|Sapphire 2026.5|S_ZDefocus
+Boris FX Sapphire|Sapphire 2026.5|S_ZFogExponential
+Boris FX Sapphire|Sapphire 2026.5|S_ZFogLinear
+Boris FX Sapphire|Sapphire 2026.5|S_ZGlow
+Boris FX Sapphire|Sapphire 2026.5|S_Zap
+Boris FX Sapphire|Sapphire 2026.5|S_ZapFrom
+Boris FX Sapphire|Sapphire 2026.5|S_ZapTo
+Boris FX Sapphire|Sapphire 2026.5|S_Zebrafy
+Boris FX Sapphire|Sapphire 2026.5|S_ZebrafyColor
+Maxon Red Giant + Universe|RG Blurs and Glows|Optical Glow
+Maxon Red Giant + Universe|RG Blurs and Glows|Shine
+Maxon Red Giant + Universe|RG Blurs and Glows|Starglow
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Blur
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Bokeh
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Chromatic Glow
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Compound Blur
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Edge Glow
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Glimmer
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Glo Fi
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Glo Fi II
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Glow
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Point Zoom
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Quantum
+Maxon Red Giant + Universe|RG Blurs and Glows|uni.Spot Blur
+Maxon Red Giant + Universe|RG Color|Colorista V
+Maxon Red Giant + Universe|RG Color|Cosmo II
+Maxon Red Giant + Universe|RG Color|Denoiser III
+Maxon Red Giant + Universe|RG Color|Film
+Maxon Red Giant + Universe|RG Color|Magic Bullet Looks
+Maxon Red Giant + Universe|RG Color|Mojo II
+Maxon Red Giant + Universe|RG Color|Parametric Curve
+Maxon Red Giant + Universe|RG Color|Renoiser
+Maxon Red Giant + Universe|RG Color|uni.Finisher
+Maxon Red Giant + Universe|RG Color|uni.Grain16
+Maxon Red Giant + Universe|RG Distortions|Chromatic Displacement
+Maxon Red Giant + Universe|RG Distortions|uni.Chromatic Aberration
+Maxon Red Giant + Universe|RG Distortions|uni.Heatwave
+Maxon Red Giant + Universe|RG Distortions|uni.Prism Displacement
+Maxon Red Giant + Universe|RG Distortions|uni.RGB Separation
+Maxon Red Giant + Universe|RG Generators|uni.Custom Dither
+Maxon Red Giant + Universe|RG Generators|uni.Fractal Background
+Maxon Red Giant + Universe|RG Generators|uni.Gradient Ramp
+Maxon Red Giant + Universe|RG Generators|uni.Soft Gradient Background
+Maxon Red Giant + Universe|RG Generators|uni.Spectralicious
+Maxon Red Giant + Universe|RG Generators|uni.Texturize
+Maxon Red Giant + Universe|RG Generators|uni.Texturize Motion
+Maxon Red Giant + Universe|RG Generators|uni.Turbulence Noise
+Maxon Red Giant + Universe|RG Motion Graphics|3D Stroke
+Maxon Red Giant + Universe|RG Motion Graphics|Echospace
+Maxon Red Giant + Universe|RG Motion Graphics|uni.Array Gun
+Maxon Red Giant + Universe|RG Motion Graphics|uni.HUD Components
+Maxon Red Giant + Universe|RG Motion Graphics|uni.Line
+Maxon Red Giant + Universe|RG Motion Graphics|uni.Logo Motion
+Maxon Red Giant + Universe|RG Motion Graphics|uni.Progresso
+Maxon Red Giant + Universe|RG Motion Graphics|uni.Reframe
+Maxon Red Giant + Universe|RG Motion Graphics|uni.Symbol Mapper
+Maxon Red Giant + Universe|RG Particles and 3D|Form
+Maxon Red Giant + Universe|RG Particles and 3D|Geo
+Maxon Red Giant + Universe|RG Particles and 3D|Horizon
+Maxon Red Giant + Universe|RG Particles and 3D|Lux
+Maxon Red Giant + Universe|RG Particles and 3D|Mir 3
+Maxon Red Giant + Universe|RG Particles and 3D|Tao
+Maxon Red Giant + Universe|RG Particles and 3D|Trapcode Particular
+Maxon Red Giant + Universe|RG Stylize|uni.Analog
+Maxon Red Giant + Universe|RG Stylize|uni.AV Club
+Maxon Red Giant + Universe|RG Stylize|uni.Carousel
+Maxon Red Giant + Universe|RG Stylize|uni.ChromaTown
+Maxon Red Giant + Universe|RG Stylize|uni.Ecto
+Maxon Red Giant + Universe|RG Stylize|uni.Electrify
+Maxon Red Giant + Universe|RG Stylize|uni.Error Diffuse Dither
+Maxon Red Giant + Universe|RG Stylize|uni.Glitch
+Maxon Red Giant + Universe|RG Stylize|uni.Halftone Dither
+Maxon Red Giant + Universe|RG Stylize|uni.Holomatrix II
+Maxon Red Giant + Universe|RG Stylize|uni.Knoll Light Factory EZ
+Maxon Red Giant + Universe|RG Stylize|uni.Long Shadow
+Maxon Red Giant + Universe|RG Stylize|uni.Luster
+Maxon Red Giant + Universe|RG Stylize|uni.MisFire
+Maxon Red Giant + Universe|RG Stylize|uni.Multitone
+Maxon Red Giant + Universe|RG Stylize|uni.Noir
+Maxon Red Giant + Universe|RG Stylize|uni.Ordered Dither
+Maxon Red Giant + Universe|RG Stylize|uni.OverLight
+Maxon Red Giant + Universe|RG Stylize|uni.RetroGrade
+Maxon Red Giant + Universe|RG Stylize|uni.ShrinkRay
+Maxon Red Giant + Universe|RG Stylize|uni.Sketchify
+Maxon Red Giant + Universe|RG Stylize|uni.Threshold Dither
+Maxon Red Giant + Universe|RG Stylize|uni.VHS
+Maxon Red Giant + Universe|RG Text|uni.Hacker Text
+Maxon Red Giant + Universe|RG Text|uni.Numbers
+Maxon Red Giant + Universe|RG Text|uni.Screen Text
+Maxon Red Giant + Universe|RG Text|uni.Text Tile
+Maxon Red Giant + Universe|RG Text|uni.Title Motion
+Maxon Red Giant + Universe|RG Text|uni.Type Cast
+Maxon Red Giant + Universe|RG Text|uni.Type On
+Maxon Red Giant + Universe|RG Text|uni.Typographic
+Maxon Red Giant + Universe|RG Transitions|uni.Blinds
+Maxon Red Giant + Universe|RG Transitions|uni.Bokeh Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Camera Shake Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Carousel Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Channel Blur
+Maxon Red Giant + Universe|RG Transitions|uni.Channel Surf
+Maxon Red Giant + Universe|RG Transitions|uni.Clock Wipe
+Maxon Red Giant + Universe|RG Transitions|uni.Color Mosaic Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Color Stripe
+Maxon Red Giant + Universe|RG Transitions|uni.Cube
+Maxon Red Giant + Universe|RG Transitions|uni.Diamond Wave
+Maxon Red Giant + Universe|RG Transitions|uni.Dolly Fade
+Maxon Red Giant + Universe|RG Transitions|uni.Exposure Blur
+Maxon Red Giant + Universe|RG Transitions|uni.Film Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Flicker Cut
+Maxon Red Giant + Universe|RG Transitions|uni.Fold
+Maxon Red Giant + Universe|RG Transitions|uni.Glitch Transition
+Maxon Red Giant + Universe|RG Transitions|uni.HalfLight
+Maxon Red Giant + Universe|RG Transitions|uni.Inside Cube
+Maxon Red Giant + Universe|RG Transitions|uni.Knoll Light Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Linear Wipe
+Maxon Red Giant + Universe|RG Transitions|uni.RetroGrade Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Rubix Cube
+Maxon Red Giant + Universe|RG Transitions|uni.Shape Wipe
+Maxon Red Giant + Universe|RG Transitions|uni.Slide
+Maxon Red Giant + Universe|RG Transitions|uni.Soft Edge Wipe
+Maxon Red Giant + Universe|RG Transitions|uni.Spectralicious Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Stretch
+Maxon Red Giant + Universe|RG Transitions|uni.Swish Pan
+Maxon Red Giant + Universe|RG Transitions|uni.Triangle Wave
+Maxon Red Giant + Universe|RG Transitions|uni.Turbulence Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Unfold
+Maxon Red Giant + Universe|RG Transitions|uni.VHS Transition
+Maxon Red Giant + Universe|RG Transitions|uni.Warp
+Maxon Red Giant + Universe|RG Utilities|Grow Bounds
+Maxon Red Giant + Universe|RG Utilities|Sound Keys
+Maxon Red Giant + Universe|RG Utilities|uni.Camera Shake
+Maxon Red Giant + Universe|RG Utilities|uni.Fisheye Fixer
+Maxon Red Giant + Universe|RG Utilities|uni.Modes
+Maxon Red Giant + Universe|RG Utilities|uni.Palettes
+Maxon Red Giant + Universe|RG Utilities|uni.Picture in Picture
+Maxon Red Giant + Universe|RG Utilities|uni.Socialize
+Maxon Red Giant + Universe|RG Utilities|uni.Unmult
+Maxon Red Giant + Universe|RG VFX|Bang
+Maxon Red Giant + Universe|RG VFX|Depth Generator
+Maxon Red Giant + Universe|RG VFX|King Pin Tracker
+Maxon Red Giant + Universe|RG VFX|Lens Distortion Matcher
+Maxon Red Giant + Universe|RG VFX|Primatte Keyer 6
+Maxon Red Giant + Universe|RG VFX|Real Lens Flares
+Maxon Red Giant + Universe|RG VFX|Reflection
+Maxon Red Giant + Universe|RG VFX|Shadow
+Maxon Red Giant + Universe|RG VFX|Spot Clone Tracker
+Maxon Red Giant + Universe|RG VFX|Supercomp
+Video Copilot|Paid / core plug-ins|Element 3D
+Video Copilot|Paid / core plug-ins|Optical Flares
+Video Copilot|Paid / core plug-ins|Twitch
+Video Copilot|Paid / core plug-ins|Heat Distortion
+Video Copilot|Free effects / plug-ins|Orb
+Video Copilot|Free effects / plug-ins|Saber
+Video Copilot|Free effects / plug-ins|Color Vibrance
+Video Copilot|Free effects / plug-ins|VC Reflect
+Video Copilot|Workflow utility (not an effect)|FX Console
+RE:Vision Effects|Current products/effects|ARRI Look Library
+RE:Vision Effects|Current products/effects|ARRI Film Lab
+RE:Vision Effects|Current products/effects|Color Genius
+RE:Vision Effects|Current products/effects|DEFlicker
+"""
+}
