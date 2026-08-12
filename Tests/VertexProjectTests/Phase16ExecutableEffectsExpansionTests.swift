33f3136fe87c03ecd518363d8a257c283236c8df
woo642778-art <woo642778@gmail.com>
2026-08-11 17:17:32 -0700

    Update executable effect count contracts for V17

--- Tests/VertexProjectTests/Phase16ExecutableEffectsExpansionTests.swift
+++ Tests/VertexProjectTests/Phase16ExecutableEffectsExpansionTests.swift
@@ -1,9 +1,9 @@
 import Testing
 import VertexProject
 
-@Test func executableEffectExpansionRequiresNinetyNineDeclaredEffects() throws {
-    #expect(ProjectEffectType.allCases.count == 99)
-    #expect(ProjectEffectDescriptorRegistry.all.count == 99)
+@Test func executableEffectExpansionRequiresOneHundredThirteenDeclaredEffects() throws {
+    #expect(ProjectEffectType.allCases.count == 113)
+    #expect(ProjectEffectDescriptorRegistry.all.count == 113)
     #expect(Set(ProjectEffectDescriptorRegistry.all.map(\.type)) == Set(ProjectEffectType.allCases))
 
     for type in ProjectEffectType.allCases {
@@ -16,5 +16,5 @@ import VertexProject
 @Test func executableEffectExpansionKeepsOnlyFourAIEffects() {
     let ai = ProjectEffectType.allCases.filter { !$0.isNativePixelEffect }
     #expect(Set(ai) == [.depthMap, .cutout, .upscale, .restore])
-    #expect(ProjectEffectType.allCases.filter(\.isNativePixelEffect).count == 95)
+    #expect(ProjectEffectType.allCases.filter(\.isNativePixelEffect).count == 109)
 }
